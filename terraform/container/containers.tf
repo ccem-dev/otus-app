569de2393a2518a782493d615a51eb9e62a3caf9
Adriano Boese <adriano@otus-solutions.com>
2020-02-07 13:10:31 -0300

    addição do player-address ao terraform

--- terraform/container/containers.tf
+++ terraform/container/containers.tf
@@ -10,6 +10,10 @@ variable "otus-app-apiurl"{
   default = "http://localhost:51002/otus-rest/v01"
 }
 
+variable "preview-address"{
+  default = "http://localhost:51001"
+}
+
 variable "otus-app-version"{
   default = "otus-app:latest"
 }
@@ -17,7 +21,7 @@ variable "otus-app-version"{
 resource "docker_container" "otus-app" {
   name = "otus-app"
   image = "${var.otus-app-version}"
-  env = ["API_URL=${var.otus-app-apiurl}"]
+  env = ["API_URL=${var.otus-app-apiurl}", "PREVIEW_ADDRESS=${var.preview-address}"]
   ports {
 	internal = 80
 	external = "${var.otus-app-port}"
