a089ebf73d96c1db1d7641c12b878d304612b121
Diogo Ferreira <diogo@otus-solutions.com.br>
2020-02-11 22:38:12 -0300

    PP-3 Correções HREF

--- infra/commands.sh
+++ infra/commands.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-envsubst '${API_URL} ${PREVIEW_ADDRESS}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf-updated
+envsubst '${API_URL} ${PREVIEW_ADDRESS} ${NGINX_HOST}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf-updated
 rm -rf /etc/nginx/conf.d/default.conf
 mv /etc/nginx/conf.d/default.conf-updated /etc/nginx/conf.d/default.conf
 exit 0;
