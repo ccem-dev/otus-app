0caffff00e63fbc109d856292493567f77270ca0
Adriano Boese <adriano@otus-solutions.com>
2020-04-27 10:11:32 -0300

    PP-4 reset de senha do participante

--- source/web/environments/environment.ts
+++ source/web/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   baseUrl: 'http://localhost:51002/otus-rest/v01',
   authBasePath: '/participant-authentication',
   activityBasePath: '/activities',
-  registerPasswordUrl: '/registerPassword',
+  registerPasswordUrl: '/participants/registerPassword',
   loginUrl: '/auth',
   logoutUrl: '/invalidate',
   followUpBasePath: '/followUp',
