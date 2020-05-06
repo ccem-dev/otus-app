23dd6bafc41710be9cdc49bf2c559af68500e0b2
Fabiano Ramires <fdrtec@gmail.com>
2020-05-06 12:04:21 -0300

    AP-5 implementação do template do componente de recuperação de senha

--- source/web/app/components/account/recovery-password/recovery-password.component.ts
+++ source/web/app/components/account/recovery-password/recovery-password.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
+import {environment} from "../../../../environments/environment";
+import {Router} from "@angular/router";
+
+const {required, email} = Validators;
 
 @Component({
-  selector: 'source-recovery-password',
+  selector: 'recovery-password',
   templateUrl: './recovery-password.component.html',
-  styleUrls: ['./recovery-password.component.css']
+  styleUrls: ['../account.scss']
 })
 export class RecoveryPasswordComponent implements OnInit {
+  title = 'Recuperação de Senha';
 
-  constructor() { }
+
+  recoveryPasswordForm: FormGroup
+  loading = false;
+  submitted = false;
+  requiredMessage = environment.requiredMessage;
+
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+  }
 
   ngOnInit() {
+    this.recoveryPasswordForm = this.formBuilder.group({
+      email: new FormControl('', [required, email])
+    });
+  }
+
+  get f(){
+    return this.recoveryPasswordForm.controls;
+
+  }
+
+  onSubmit() {
+    let email = this.f.email.value.toLowerCase();
+  }
+
+  redirectToLogin(){
+    this.router.navigate(['/login']);
+
   }
 
 }
