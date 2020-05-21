06e435dda070db477dfc9a49fef7bfb2f3a3eeb7
Fabiano Ramires <fdrtec@gmail.com>
2020-05-21 11:19:32 -0300

    AP-6 implementação do primeiro otus input com validações reutilizavel

---
+++ source/web/app/shared/components/fields/input-text-area/input-text-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'otus-input-text-area',
+  templateUrl: './input-text-area.component.html',
+  styleUrls: ['./input-text-area.component.css']
+})
+export class InputTextAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ source/web/app/shared/components/fields/input-text/input-text.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup} from '@angular/forms';
+import {FieldValidatorService} from '../field-validator.service';
+
+@Component({
+  selector: 'otus-input-text',
+  templateUrl: './input-text.component.html',
+  styleUrls: ['./input-text.component.css']
+})
+export class InputTextComponent {
+
+  @Input() title: string;
+  @Input() formGroup: FormGroup;
+  @Input() controlName: string;
+
+  constructor(public fieldValidator: FieldValidatorService) { }
+
+  get formControl(): AbstractControl {
+    return this.formGroup.controls[this.controlName];
+  }
+
+}
