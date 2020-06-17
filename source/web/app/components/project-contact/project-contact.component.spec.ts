861875eea90e3e44aa6e5b5cb8e753a622260025
Fabiano Ramires <fdrtec@gmail.com>
2020-06-17 10:02:33 -0300

    AP-6 UnitTest com mock de valores de form, verificação de atributo privado e tratamento de observable

--- source/web/app/components/project-contact/project-contact.component.spec.ts
+++ source/web/app/components/project-contact/project-contact.component.spec.ts
@@ -12,19 +12,25 @@ import {Router} from "@angular/router";
 import {MockValues} from "../../shared/mocks/mock-values";
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatSnackBar, MatTooltipModule} from '@angular/material';
+import {OtusToasterService} from "../../shared/services/otus-toaster.service";
+import {BehaviorSubject} from "rxjs";
+import {group} from "@angular/animations";
 
-describe('ProjectContactItemComponent', () => {
+describe('ProjectContactComponent', () => {
   let component: ProjectContactComponent;
   let fixture: ComponentFixture<ProjectContactComponent>;
   let projectContactService: ProjectContactService;
   let router: Router;
+  let otusToasterService: OtusToasterService;
+  let Mock: any = {};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ProjectContactComponent],
       imports: [
         MatChipsModule,
-        MatIconModule, RouterTestingModule,
+        MatIconModule,
+        RouterTestingModule,
         HttpClientTestingModule,
         ReactiveFormsModule,
         MatTooltipModule
@@ -43,7 +49,9 @@ describe('ProjectContactItemComponent', () => {
     fixture = TestBed.createComponent(ProjectContactComponent);
     component = fixture.componentInstance;
     projectContactService = fixture.debugElement.injector.get(ProjectContactService);
+    otusToasterService = fixture.debugElement.injector.get(OtusToasterService);
     router = fixture.debugElement.injector.get(Router);
+    initializeMocks();
 
    // @ts-ignore
     spyOn(router, 'getCurrentNavigation').and.returnValue({
@@ -55,6 +63,31 @@ describe('ProjectContactItemComponent', () => {
   });
 
   it('should create', () => {
-
+    expect(component).toBeTruthy();
   });
+
+  it('onSubmit method with validForm should simulate recording a Issue (ProjectContact)', function () {
+    spyOn(projectContactService, "createProjectContact").and.returnValue(Mock.pcs.returnCreateProjectContact);
+    spyOn(component, 'getProjectContacts')
+    spyOn(component, "changeViewCallFormState").and.callThrough();
+    spyOn(otusToasterService, "showMessage");
+    component["projectContactForm"].patchValue({["title"]:"mockTitle"});
+    component["projectContactForm"].patchValue({["text"]:"MockText"});
+    component.onSubmit()
+    expect(component.getProjectContacts).toHaveBeenCalledTimes(1);
+    expect(component.changeViewCallFormState).toHaveBeenCalledTimes(1);
+    expect(otusToasterService.showMessage).toHaveBeenCalledTimes(1);
+  });
+
+  function initializeMocks() {
+    Mock = {
+      pcs:
+        {
+          returnCreateProjectContact: new BehaviorSubject(true),
+          // returnGetLastMessageNotFound: new BehaviorSubject([])
+        }
+    };
+  }
+
+
 });
