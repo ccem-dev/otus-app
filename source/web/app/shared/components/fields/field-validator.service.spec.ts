1ec0e3418df57a3d12fc412e85e874deb314de58
Fabiano Ramires <fdrtec@gmail.com>
2020-06-17 16:40:19 -0300

    AP-6 teste unitário para service de validação dos inputs

--- source/web/app/shared/components/fields/field-validator.service.spec.ts
+++ source/web/app/shared/components/fields/field-validator.service.spec.ts
@@ -1,12 +1,26 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { FieldValidatorService } from './field-validator.service';
+import {FieldValidatorService} from './field-validator.service';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 describe('FieldValidatorService', () => {
+  let service: FieldValidatorService
+  let mockForm: FormGroup;
+  let fb: FormBuilder = new FormBuilder();
+
   beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => {
+    service = TestBed.get(FieldValidatorService);
+    mockForm = fb.group({
+      title: ["", [Validators.required]]
+    });
+  })
 
   it('should be created', () => {
-    const service: FieldValidatorService = TestBed.get(FieldValidatorService);
     expect(service).toBeTruthy();
+  });
+
+  it('hasErrorValidate should ', function () {
+    expect(service.hasErrorValidate(mockForm, "required")).toBeFalse();
   });
 });
