02c05e05ea35b39e441839a70a75d364460d6401
Fabiano Ramires <fdrtec@gmail.com>
2020-06-27 10:41:50 -0300

    PP-2 removendo push do pipeline de stream

--- source/web/app/components/exam-results/exam-results.component.ts
+++ source/web/app/components/exam-results/exam-results.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit, Output} from '@angular/core';
 import {ExamResultsService} from '../../providers/exam-results/exam-results.service';
 import {OwnerService} from '../../shared/owner/owner.service';
 import {Report} from '../../model/exam-results/report';
+import {Subscription} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 
 @Component({
@@ -15,6 +17,7 @@ export class ExamResultsComponent implements OnInit {
   //@Output() reports$: Observable<Report[]>;
   @Output() owner;
   @Output() loading: boolean = false;
+  private examResults$: Subscription;
 
   constructor(private examResultsService: ExamResultsService,
               private ownerService: OwnerService) {
@@ -25,6 +28,7 @@ export class ExamResultsComponent implements OnInit {
     this.getReportByParticipant(this.owner);
   }
 
+
   createPDF() {
     // const doc = new jsPDF()
     // const element = document.querySelector('div')
@@ -32,12 +36,26 @@ export class ExamResultsComponent implements OnInit {
     // doc.save()
   }
 
+  ngOnDestroy() {
+    this.examResults$.unsubscribe();
+  }
+
+
+  // private getReportByParticipant(ownerRn: string): void {
+  //   // @ts-ignore
+  //   this.examResultsService.getReportByParticipant(ownerRn)
+  //     .toPromise()
+  //     .then((result) => this.reports = result)
+  //     .catch(e => console.log("erro captado: "+ e))
+  // }
+
   private getReportByParticipant(ownerRn: string): void {
-    // @ts-ignore
-    this.examResultsService.getReportByParticipant(ownerRn)
+    this.examResults$ = this.examResultsService.getReportByParticipant(ownerRn)
+      .pipe(tap(result => console.log(result)))
       .subscribe(reportFull => [
-        this.reports = reportFull,
-        this.loading = false]
+          this.reports = reportFull,
+          this.loading = false],
+        // error => console.log(error)
       );
   }
 
--- source/web/app/providers/exam-results/exam-results.service.ts
+++ source/web/app/providers/exam-results/exam-results.service.ts
@@ -1,24 +1,53 @@
 import {Injectable} from '@angular/core';
 import {Report} from '../../model/exam-results/report';
 import {ExamClientService} from '../rest/exam-results/exam-client.service';
-import {concatMap, map, mergeMap} from 'rxjs/operators';
+import {concatMap, map, mergeMap, tap, toArray} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExamResultsService {
+  // }
+  private invalidResponse: Observable<any>;
+
+  // getReportByParticipant(ownerRn: string) {
+  //   let examList = [];
+  //   let reportFull$ = this.examClientService.getReportByParticipant(ownerRn)
+  //     .pipe(
+  //       concatMap(response => response['data']),
+  //       mergeMap(report => this.examClientService.getFullReport(ownerRn, report['_id'])),
+  //       tap((item) => console.log(item)),
+  //       map(item => examList.push(new Report(item.data))),
+  //       map(() => examList)
+  //     );
+  //   return reportFull$;
+
   constructor(private examClientService: ExamClientService) {
   }
 
   getReportByParticipant(ownerRn: string) {
-    let examList = [];
-    let reportFull$ = this.examClientService.getReportByParticipant(ownerRn)
+    return this.examClientService.getReportByParticipant(ownerRn)
       .pipe(
+        // tap(ver1 => console.log(ver1)),
         concatMap(response => response['data']),
-        mergeMap(report => this.examClientService.getFullReport(ownerRn, report['_id'])),
-        map(item => examList.push(new Report(item.data))),
-        map(() => examList)
+        mergeMap(report => {
+          console.log(report['label']);
+          if ((report['label']) !== 'Retinografia') {
+            return this.examClientService.getFullReport(ownerRn, report['_id']);
+          } else return [{data: {}}]
+        }),
+        //tap((ver2 => console.log(ver2))),
+        map(item => this.isValidReport(item.data)),
+        toArray()
       );
-    return reportFull$;
+  }
+
+  isValidReport(candidate) {
+    try {
+      return new Report(candidate);
+    } catch (e) {
+      console.log('não é reportValido');
+    }
   }
 }
